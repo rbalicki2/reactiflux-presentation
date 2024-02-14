81a3c246d9e4a8049679630f2b438bd25e8df089
Robert Balicki <robert.balicki@gmail.com>
2024-02-14 14:06:25 -0500

    initial commit

---
+++ presentation/theme.tsx
@@ -0,0 +1,14 @@
+import { themes } from "mdx-deck";
+
+export default {
+  ...themes.highlight,
+  colors: {
+    background: "white",
+    primary: "#4e9ebc",
+    text: "black",
+  },
+  fonts: {
+    body: "Maison",
+    heading: "GTEesti",
+  },
+};
